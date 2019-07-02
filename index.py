3fcfedf9c4953e708a8c4645caa16308a636e658
ryakhov <33357917+ryakhov@users.noreply.github.com>
2019-07-02 21:32:49 +0500

    Добавил задачу hard
    Добавил задачу hard

--- index.py
+++ index.py
@@ -18,3 +18,24 @@ number1, number2 = number2, number1
 
 print(number1)
 print(number2)
+
+#Задача Hard - на скорую руку. Я понимаю, что тут лучше использовать словарь, но не успел разобраться
+
+name = input('Ваши имя и фамилия: ')
+while len(name) < 2:
+    print('Введите пожалуйста Имя и Фамилию')
+    name = input('Ваши имя и фамилия: ')
+
+age = int(input('Ваш возраст: '))
+weight = int(input('Ваш вес: '))
+
+med_data = []
+med_data.append([name, age, weight])
+print()
+if age < 30 and weight > 50 < 120:
+    print('Уважаемый {}, {} год, вес {}'.format(med_data[0][0], med_data[0][1], med_data[0][2]),' - хорошее состояние')
+elif age > 40 and weight < 50 or weight > 120:
+    print('Уважаемый {}, {} год, вес {}'.format(med_data[0][0], med_data[0][1], med_data[0][2]),' - следует обратится к врачу!')
+elif age > 30 and weight < 50 or weight > 120:
+    print('Уважаемый {}, {} год, вес {}'.format(med_data[0][0], med_data[0][1], med_data[0][2]),' - следует заняться собой')
+else: print('Уважаемый {}, {} год, вес {}'.format(med_data[0][0], med_data[0][1], med_data[0][2]),' - Вы не в нашей компетенции')
